3058336c6c33c30c28f593fb32c39f483745d785
emilmff <emil909@ymail.com>
2022-12-17 22:48:35 +0100

    adsdasd

--- ttt.cpp
+++ ttt.cpp
@@ -1,17 +1,26 @@
 #include"ttt.h"
 
-board::board(int starting_turn, int s)
+int change(int turn) {
+	return -turn;
+}
+
+board::board(int s)
 {
 	size = s;
-	for (int i = 0; i < size; i++)
-		data.push_back(0);
-	turn = starting_turn;
+	data = std::vector<int>(s*s, 0);
+	turn = -1;
 }
 
 board::board() {
 	size = 3;
-	data = { 0,0,0,0,0,0,0,0,0 };
-	turn = BLACK;
+	data = std::vector<int>(9, 0);
+	turn = -1;
+}
+
+board::board(std::vector<int> board) {
+	data = board;
+	size = (int)std::sqrt((double)data.size());
+	turn = -1;
 }
 board* board::set(int r, int c, int val)
 {
@@ -19,9 +28,19 @@ board* board::set(int r, int c, int val)
 	return this;
 }
 
+board* board::change_turn() {
+	turn = change(turn);
+	return this;
+}
+
 int board::get(int r, int c)
 {
 	return data.at(r * size + c);
+}
+
+int board::get_turn()
+{
+	return turn;
 }
 
 board* board::print()
@@ -31,9 +50,9 @@ board* board::print()
 		for (int j = 0; j < size; j++)
 		{
 			if (data.at(i * size + j) == -1)
-				std::cout << "x";
+				std::cout << "X";
 			else if (data.at(i * size + j) == 1)
-				std::cout << "o";
+				std::cout << "O";
 			else if (data.at(i * size + j) == 0)
 				std::cout << ".";
 		}
@@ -45,7 +64,8 @@ board* board::print()
 
 bool board::is_valid_move(int r, int c)
 {
-	return (data.at(r * size + c) == 0) ? true : false;
+	if (r < size && c < size) return (data.at(r * size + c) == 0) ? true : false;
+	else return false;
 }
 
 std::vector<std::vector<int>> board::get_valid_locations()
@@ -64,19 +84,19 @@ std::vector<std::vector<int>> board::get_valid_locations()
 	return out;
 }
 
-std::vector<int> board::search_direction(int r_s, int c_s, int r_d, int c_d)
+std::vector<int> board::search_direction(int r_s, int c_s, int r_d, int c_d,int distance)
 {
 	std::vector<int> out;
 
-	for (int i = 1; ((r_s + r_d * i) < size)&& ((r_s + r_d * i) > -1) && ((c_s + c_d * i) < size )&& ((c_s + c_d * i) > -1); i++)
+	for (int i = 0; ((r_s + r_d * i) < size) && ((r_s + r_d * i) > -1) && ((c_s + c_d * i) < size ) && ((c_s + c_d * i) > -1) && (i < distance+1); i++)
 		out.push_back(data.at((r_s + r_d * i) * size + c_s + c_d * i));
 	return out;
 }
 
-std::vector<int> board::get_end(int r_s, int c_s, int r_d, int c_d)
+std::vector<int> board::get_end(int r_s, int c_s, int r_d, int c_d, int distance)
 {
 	std::vector<int> out = { r_s,c_s };
-	for (int i = 1; ((r_s + r_d * i) < size) && ((r_s + r_d * i) > -1) && ((c_s + c_d * i) < size) && ((c_s + c_d * i) > -1); i++)
+	for (int i = 1; ((r_s + r_d * i) < size) && ((r_s + r_d * i) > -1) && ((c_s + c_d * i) < size) && ((c_s + c_d * i) > -1) && (i<distance +1); i++)
 		out = { r_s + r_d * i,c_s + c_d * i };
 	return out;
 }
@@ -97,18 +117,16 @@ std::vector<std::vector<int>> board::get_involved_dirs(int r, int c)
 	std::vector<std::vector<int>> dirs;
 	for (auto& i : major_dirs)
 	{
-		std::vector<int> start = get_end(r, c, -i.at(0), -i.at(1));
-		std::vector<int> dir = search_direction(start.at(0), start.at(1), i.at(0), i.at(1));
-		dir.insert(dir.begin(),  get(r,c));
+		std::vector<int> start = get_end(r, c, -i.at(0), -i.at(1),2);
+		std::vector<int> dir = search_direction(start.at(0), start.at(1), i.at(0), i.at(1),4);
 		dirs.push_back(dir);
 	}
 	return dirs;
 }
 
-bool board::is_end(int r, int c)
+int board::is_end(int r, int c)
 {
-	std::vector<std::vector<int>> dirs = get_involved_dirs(r,c);
-
+	std::vector<std::vector<int>> dirs =get_involved_dirs(r,c);
 	for (auto& i : dirs)
 	{
 		int prev = 0;
@@ -121,7 +139,7 @@ bool board::is_end(int r, int c)
 					len += 1;
 					if (len == to_lose)
 					{
-						return true;
+						return prev;
 					}
 				}
 				else
@@ -136,13 +154,240 @@ bool board::is_end(int r, int c)
 			}
 		}
 	}
-	return false;
+	return 0;
 }
+
+board* board::play_turn(int r, int c)
+{
+	if (is_valid_move(r, c))
+	{
+		set(r, c, turn);
+	}
+	return this;
+}
+
+engine::engine(player* player1,player* player2) {
+	b = new board;
+	p1 = player1;
+	p2 = player2;
+}
+
+engine::engine(player* player1, player* player2,int size) {
+	b = new board(size);
+	p1 = player1;
+	p2 = player2;
+}
+
+engine::engine(board* bo, player* player1, player* player2) {
+	b = bo;
+	p1 = player1;
+	p2 = player2;
+}
+
+void engine::print_board()
+{
+	b->print();
+}
+
+player* engine::get_player()
+{
+	if (b->get_turn() == -1)
+	{
+		return p1;
+	}
+	else {
+		return p2;
+	}
+}
+
+int engine::game_loop(bool print)
+{
+	while (1)
+	{
+		if(print)b->print();
+		std::vector<int> move = get_player()->get_turn(b);
+		b->play_turn(move[0], move[1]);
+		if (int i = b->is_end(move[0],move[1])) return i;
+		else if (b->get_valid_locations().size() == 0) return 2;
+		b->change_turn();
+	}
+}
+
+std::vector<int> human_player::get_turn(board* b)
+{
+	int r, c;
+	while (1) {
+		std::cin >> r >> c;
+		if(b->is_valid_move(r,c)) return { r, c };
+	}
+}
+
+minimax_player::minimax_player(int d)
+{
+	depth = d;
+}
+
+std::vector<int> minimax_player::minimax(board* b,int dep, int alpha, int beta,bool maximizing, int prev_row, int prev_col) {
+	if (prev_row != -1) {
+		if (int i = b->is_end(prev_row, prev_col))
+		{
+			if (i == change(b->get_turn())) return maximizing ? std::vector<int>({ 1, -1,-1 }) : std::vector<int>({ -1, -1,-1 });
+			else return { 0,-1,-1 };
+		}
+		if (dep == 0) return{ 0,-1, -1 };
+	}
+	int val;
+	std::vector<int> move;
+	if (maximizing)
+	{
+		val = -2;
+		std::vector<std::vector<int>> possible_moves = b->get_valid_locations();
+		if (possible_moves.size() == 0) return { 0,-1,-1 };
+		move = possible_moves[0];
+
+		for (auto m : possible_moves)
+		{
+			int r = m[0], c = m[1];
+			b->play_turn(r, c);
+			int new_val = minimax(b->change_turn(), dep - 1, alpha, beta, false, r, c)[0];
+			b->change_turn();
+			b->set(r, c, 0);
+			if (new_val > val)
+			{
+				val = new_val;
+				move = {r,c};
+			}
+			alpha = std::max(alpha, val);
+			if (alpha >= beta) break;
+		}
+	}
+	else {
+		val = 2;
+		std::vector<std::vector<int>> possible_moves = b->get_valid_locations();
+		if (possible_moves.size() == 0) return { 0,-1,-1 };
+		move = possible_moves[0];
+
+		for (auto m : possible_moves)
+		{
+			int r = m[0], c = m[1];
+			b->play_turn(r, c);
+			int new_val = minimax(b->change_turn(), dep - 1, alpha, beta, true, r, c)[0];
+			b->change_turn();
+			b->set(r, c, 0);
+			if (new_val < val)
+			{
+				val = new_val;
+				move = { r,c };
+			}
+			beta = std::min(beta, val);
+			if (alpha >= beta) break;
+		}
+	}
+	return { val,move[0],move[1] };
+}
+
+std::vector<int> minimax_player::get_turn(board* b)
+{
+	std::vector<int> val_move = minimax(b, depth, -2, 2, true, -1, -1);
+	return { val_move[1],val_move[2] };
+}
+
+void play(char player)
+{
+	int size = 0;
+	std::cout << "please enter what size board you'd like to play on:";
+	while (size < 3)
+	{
+		std::cin >> size;
+		if (size < 3)std::cout << "try again";
+	}
+	int depth = 0;
+	std::cout << "please enter what depth of minimax you'd like to play against:";
+	while (depth <1)
+	{
+		std::cin >> depth;
+		if (size < 1)std::cout << "try again";
+	}
+
+	engine* game = new engine(new minimax_player(depth), new human_player, size);
+
+	if (player == 'X')
+	{
+		game = new engine(new human_player, new minimax_player(depth), size);
+	}
+	std::cout << "you play by typing in 2 numbers : the first being the row you want to play in, second one the column" << "\n";
+	int result = game->game_loop(true);
+	game->print_board();
+	switch (result)
+	{
+	case(-1):
+		if (player != 'X') std::cout << "you won!";
+		else std::cout << "haha you lost!";
+		break;
+	case(1):
+		if (player == 'X') std::cout << "you won!";
+		else std::cout << "haha you lost!";
+		break;
+	case(2):
+		std::cout << "draw";
+		break;
+	}
+}
+
+void play(int num)
+{
+	while (num > 0)
+	{
+		std::vector<int> data(9,0);
+		int num_x = 0;
+		int num_o = 0;
+		for (int i = 0; i < 9; i++)
+		{
+			char next;
+			std::cin >> next;
+			if (next == 'x')
+			{ 
+				data[i] = -1;
+				num_x++;
+			}
+			else if (next == 'o')
+			{
+				data[i] = 1;
+				num_o++;
+			}
+		}
+		board* b = new board(data);
+		if (num_x > num_o)
+		{
+			b->change_turn();
+		}
+		engine* game = new engine(b, new minimax_player(9), new minimax_player(9));
+		int result = game->game_loop(false);
+		switch (result)
+		{
+		case -1:
+			std::cout << "O";
+			break;
+		case 1:
+			std::cout << "X";
+			break;
+		case 2:
+			std::cout << "N";
+		}
+		num--;
+	}
+}
+
 int main() {
-	board* b = new board();
-	std::cout << b->print()->is_end(0,0)<<std::endl;
-	b->set(0, 0, 1)->set(1,1,1)->set(2,2,1)->print();
-	std::cout << b->is_end(0,0);
-	
+	std::string s;
+	std::cin >> s;
+	if (isdigit(s[0]))
+	{
+		play(stoi(s));
+	}
+	else
+	{
+		play(s[0]);
+	}
 	return 0;
 };
--- ttt.h
+++ ttt.h
@@ -1,7 +1,9 @@
 #pragma once
 #include <iostream>
 #include <vector>
-
+#include <algorithm>
+#include <string>
+#include <cmath>
 int change_turn(int turn);
 
 class board {
@@ -9,45 +11,59 @@ class board {
 	int size;
 	int turn;
 	static constexpr int to_lose = 3;
-	static constexpr int WHITE = 1;
-	static constexpr int BLACK = -1;
 public:
 	board();
-	board(int starting_turn, int s);
+	board(int s);
+	board(std::vector<int>);
 	board* set(int r, int c, int val);
+	board* change_turn();
 	int get(int r, int c);
+	int get_turn();
 	board* print();
 	bool is_valid_move(int r, int c);
 	std::vector<std::vector<int>> get_valid_locations();
-	std::vector<int> search_direction(int, int, int, int);
+	std::vector<int> search_direction(int, int, int, int, int);
 	static std::vector<std::vector<int>> get_major_dirs();
 	std::vector<std::vector<int>> get_involved_dirs(int, int);
-	std::vector<int> get_end(int, int, int, int);
-	bool is_end(int,int);
+	std::vector<int> get_end(int, int, int, int,int distance);
+	int is_end(int,int);
 	board* play_turn(int r, int c);
 };
 
+class player;
+
 class engine {
 	board* b;
-
-	engine();
+	player* p1;
+	player* p2;
+public:
+	engine(player*,player*);
+	engine(player*, player*,int);
+	engine(board*, player*, player*);
 	engine(int starting_turn, int s);
 
-	int game_loop();
-	std::vector<int> get_turn();
+	void print_board();
+	player* get_player();
+	int game_loop(bool print);
 };
 
 class player{
-protected:	
-	int color;
-	player(int c);
-	virtual std::vector<int> get_turn() = 0;
+public:
+	virtual std::vector<int> get_turn(board*) = 0;
 };
 
 class human_player : public player {
-	human_player(int c);
-	virtual std::vector<int> get_turn();
+public:
+	virtual std::vector<int> get_turn(board*);
 };
-class minimax {
 
+class minimax_player : public player {
+	int depth;
+	std::vector<int> minimax(board*,int,int,int,bool,int,int);
+public:
+	minimax_player(int);
+	virtual std::vector<int> get_turn(board*);
 };
+
+void play(char);
+void play(int);
